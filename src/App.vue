aa8b40c5430feeae90e2adf416239800021c0e1c
AmosCheruiyot01 <cheruiyotamos728@gmail.com>
2023-05-04 08:32:42 +0300

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<section>
+    <h2>my friends</h2> 
+<ul>
+    <li></li>
+</ul>
+</section>
+</template>
+
+<script>export default {
+    data(){
+        return{
+            friends:[
+                { id:'amos',
+                phone:'0717239483',
+                email:'guru@gmail.com'},
+
+                { id:'julie',
+                phone:'0717238483',
+                email:'julie@gmail.com'}
+            ]
+            
+        }
+    }
+}</script>
